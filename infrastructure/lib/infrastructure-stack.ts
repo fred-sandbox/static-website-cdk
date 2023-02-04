be9d480194eff731693e1819c7a82908e46ffbdd
crochetcode <fred.davison@outlook.com>
2023-02-04 10:22:48 -0600

    add bucket and bucket deployment

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,16 +1,20 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from '@aws-cdk/core';
+import * as s3 from '@aws-cdk/aws-s3';
+import * as s3Deployment from '@aws-cdk/aws-s3-deployment';
 
-export class InfrastructureStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+export class InfrastructureStack extends cdk.Stack {
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
+        const myBucket = new s3.Bucket(this, 'website-bucket', {
+            publicReadAccess: true,
+            removalPolicy: cdk.RemovalPolicy.DESTROY,
+            websiteIndexDocument: 'index.html'
+        });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfrastructureQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+        const deployment = new s3Deployment.BucketDeployment(this, 'deployStaticWebsite', {
+            sources: [s3Deployment.Source.asset('../website')],
+            destinationBucket: myBucket
+        });
+    }
 }
